f8f5b45b51d9f189b3ccc37827abc6fb57f5d6ef
Dan Pastusek <dan@hexial.com>
2022-03-25 16:08:15 -0600

    Update provision-os-step2.sh

--- provision-os-step2.sh
+++ provision-os-step2.sh
@@ -6,26 +6,12 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
-# Install K3s
-if [[ ! -d /var/lib/rancher/k3s/data ]]; then
-  curl -sfL https://get.k3s.io | INSTALL_K3S_CHANNEL=stable sh
-fi
-
 # Install helm
 curl -sLo helm.tar.gz https://get.helm.sh/helm-v3.7.2-linux-arm64.tar.gz
 tar zxf helm.tar.gz
 mv linux-arm64/helm /usr/local/bin/
 chmod +x /usr/local/bin/helm
 rm -rf linux-arm64 helm.tar.gz
-
-# Stop K3s, remove certs to get regenerated on next boot
-k3s kubectl --insecure-skip-tls-verify -n kube-system delete secret k3s-serving
-service k3s stop
-rm -vrf /var/lib/rancher/k3s/agent/*.key \
-    /var/lib/rancher/k3s/agent/*.crt \
-    /etc/rancher/k3s/k3s.yaml \
-    /var/lib/rancher/k3s/server/token \
-    /var/lib/rancher/k3s/server/tls
 
 # Pibox Disk Provisioner - Note, this script will potentially format attached disks. Careful!
 mkdir -p /opt/kubesail/
