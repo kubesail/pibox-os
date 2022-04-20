6d073df09fd6359375c11f42c6aa9bbaacef6e67
Dan Pastusek <dan@hexial.com>
2022-04-19 21:59:50 -0600

    save overlay

--- setup-display.sh
+++ setup-display.sh
@@ -4,7 +4,8 @@ make
 mv /lib/modules/"$(uname -r)"/kernel/drivers/staging/fbtft/fb_st7789v.ko /lib/modules/"$(uname -r)"/kernel/drivers/staging/fbtft/fb_st7789v.BACK
 mv fb_st7789v.ko /lib/modules/"$(uname -r)"/kernel/drivers/staging/fbtft/fb_st7789v.ko
 popd
-dtc --warning no-unit_address_vs_reg -I dts -O dtb -o /boot/overlays/drm-minipitft13.dtbo overlays/minipitft13-overlay.dts
+curl https://raw.githubusercontent.com/kubesail/pibox-os/main/overlays/minipitft13-overlay.dts -o /tmp/overlays/minipitft13-overlay.dts
+dtc --warning no-unit_address_vs_reg -I dts -O dtb -o /boot/overlays/drm-minipitft13.dtbo /tmp/overlays/minipitft13-overlay.dts
 sed -i 's/$/ fbcon=map:1/' /boot/cmdline.txt
 cat <<EOF >> /boot/config.txt
 dtoverlay=spi0-1cs
