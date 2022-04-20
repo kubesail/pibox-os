74f7c18d7c2bac4e8d5cbb7cb403a2bf9f8f1af5
Dan Pastusek <dan@hexial.com>
2022-04-20 12:51:20 -0600

    combine provision script

--- provision-os.sh
+++ provision-os.sh
@@ -63,4 +63,50 @@ dphys-swapfile swapoff
 sysctl -w vm.swappiness=1
 sed -i 's/vm.swappiness=.*/vm.swappiness=1/' /etc/sysctl.conf
 
-# reboot -f
+#!/bin/bash
+set -e
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Install helm
+curl -sLo helm.tar.gz https://get.helm.sh/helm-v3.7.2-linux-arm64.tar.gz
+tar zxf helm.tar.gz
+mv linux-arm64/helm /usr/local/bin/
+chmod +x /usr/local/bin/helm
+rm -rf linux-arm64 helm.tar.gz
+
+# Pibox Disk Provisioner - Note, this script will potentially format attached disks. Careful!
+mkdir -p /opt/kubesail/
+curl -sLo /opt/kubesail/provision-disk.sh https://raw.githubusercontent.com/kubesail/pibox-os/main/provision-disk.sh
+chmod +x /opt/kubesail/provision-disk.sh
+/opt/kubesail/./provision-disk.sh
+# Run disk provisioner before K3s starts
+mkdir -p /etc/systemd/system/k3s.service.d
+echo -e "[Service]\nExecStartPre=/opt/kubesail/provision-disk.sh" > /etc/systemd/system/k3s.service.d/override.conf
+systemctl daemon-reload
+
+# Install KubeSail helper services
+curl -s https://raw.githubusercontent.com/kubesail/pibox-os/main/setup.sh | bash
+# now you can run `kubesail` to initialize the KubeSail agent at any time
+
+# Refresh SSH certs on first boot
+touch /boot/refresh-ssh-certs
+
+# This happens with PiShrink. Only uncomment if using packer
+# truncate -s-1 /boot/cmdline.txt
+# echo -n " init=/usr/lib/raspi-config/init_resize.sh" >> /boot/cmdline.txt
+
+# Disable SSH
+#systemctl stop ssh
+#systemctl disable ssh
+
+# Clean bash history
+history -c && history -w
+exit
+
+# logout of root and do the same for pi user
+history -c && history -w
+exit
--- setup.sh
+++ setup.sh
@@ -3,37 +3,6 @@
 useradd -u 989 --system --shell=/usr/sbin/nologin kubesail-agent
 mkdir -p /opt/kubesail/
 
-# Install KubeSail init script
-cat <<'EOF' > /opt/kubesail/init.sh
-#!/bin/bash
-if [[ ! -f /boot/kubesail-username.txt ]]; then
-    echo "Not installing KubeSail agent: /boot/kubesail-username.txt file not found"
-    exit 0
-fi
-
-APISERVER_TIMEOUT=60 # Wait n seconds for k3s apiserver to start
-for i in $(seq 1 $APISERVER_TIMEOUT); do
-    APISERVER_STATUS="$(curl -s -o /dev/null -w "%{http_code}" --max-time 5 -k https://localhost:6443)"
-    echo $APISERVER_STATUS
-    if [ $APISERVER_STATUS == "401" ]; then
-        break
-    fi
-    sleep 1
-done
-if [ $APISERVER_STATUS != "401" ]; then
-    echo "Timeout waiting for k3s apiserver to start"
-    exit 1
-fi
-
-KUBESAIL_USERNAME=$(cat /boot/kubesail-username.txt)
-echo "Installing KubeSail agent with username: $KUBESAIL_USERNAME"
-
-k3s kubectl get namespace kubesail-agent || {
-    k3s kubectl create -f https://byoc.kubesail.com/$KUBESAIL_USERNAME.yaml?initialID=PiBox
-}
-EOF
-chmod +x /opt/kubesail/init.sh
-
 # Install KubeSail CLI setup script
 cat <<'EOF' > /usr/local/bin/kubesail
 #!/bin/bash
