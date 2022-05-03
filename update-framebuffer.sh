f70d0b059a1ba0c028c3e14ac2d5ff768ffab0b1
Dan Pastusek <dan@hexial.com>
2022-05-03 11:33:08 -0500

    Update update-framebuffer.sh

--- update-framebuffer.sh
+++ update-framebuffer.sh
@@ -8,6 +8,7 @@ service pibox-framebuffer stop
 
 FB_VERSION=v8
 FB_PATH=/opt/kubesail/pibox-framebuffer-$FB_VERSION
+rm -f $FB_PATH
 echo "downloading pibox-framebuffer $FB_VERSION"
 if [[ ! -f $FB_PATH ]]; then
     curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer
