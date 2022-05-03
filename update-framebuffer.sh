7fa12d86b13fed5aae534d839e96507f272fb060
Dan Pastusek <dan@hexial.com>
2022-05-03 11:20:52 -0500

    Create update-framebuffer.sh

---
+++ update-framebuffer.sh
@@ -0,0 +1,14 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+FB_VERSION=v8
+FB_PATH=/opt/kubesail/pibox-framebuffer-$FB_VERSION
+if [[ ! -f $FB_PATH ]]; then
+    curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer
+    chmod +x $FB_PATH
+    rm /opt/kubesail/pibox-framebuffer
+    ln -s $FB_PATH /opt/kubesail/pibox-framebuffer
+fi
+chown -R kubesail-agent: /opt/kubesail/
