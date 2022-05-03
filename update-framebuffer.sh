ed32a5a18ea1c5ec9b786fe159fcb546eaf19cdb
Dan Pastusek <dan@hexial.com>
2022-05-03 11:29:39 -0500

    Update update-framebuffer.sh

--- update-framebuffer.sh
+++ update-framebuffer.sh
@@ -3,8 +3,12 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+echo "stopping pibox-framebuffer service"
+service pibox-framebuffer stop
+
 FB_VERSION=v8
 FB_PATH=/opt/kubesail/pibox-framebuffer-$FB_VERSION
+echo "downloading pibox-framebuffer $FB_VERSION"
 if [[ ! -f $FB_PATH ]]; then
     curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer
     chmod +x $FB_PATH
@@ -13,3 +17,5 @@ if [[ ! -f $FB_PATH ]]; then
 fi
 chown -R kubesail-agent: /opt/kubesail/
 ls -alh /opt/kubesail/pibox-framebuffer
+echo "starting pibox-framebuffer service"
+service pibox-framebuffer start
