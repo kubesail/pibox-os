a613df172925c73bb923995cfbff43bbe9dbf608
Dan Pastusek <dan@hexial.com>
2022-05-17 18:35:45 -0500

    write k3s install status to framebuffer

--- setup.sh
+++ setup.sh
@@ -38,7 +38,41 @@ fi
 # Install K3s and KubeSail agent
 if [[ ! -d /var/lib/rancher/k3s/data ]]; then
   echo "Installing k3s and KubeSail agent"
+  curl --unix-socket /var/run/pibox/framebuffer.sock "http://localhost/text?content=Installing+k3s&background=000000&size=46&color=ffffff"
   curl --connect-timeout 10 --retry 5 --retry-delay 3 -L https://get.k3s.io | INSTALL_K3S_CHANNEL=stable sh
+
+  function kubernetes_failed_to_boot () {
+    curl --unix-socket /var/run/pibox/framebuffer.sock "http://localhost/text?content=Failed+starting+k3s+services&background=000000&size=46&color=ff0000"
+    kill $SCREEN_TIMER_PID
+    exit 1
+  }
+
+  function screen_timer () {
+    i=1
+    while true
+    do
+      curl --unix-socket /var/run/pibox/framebuffer.sock "http://localhost/text?content=Waiting+for+k3s+services&background=000000&size=46&color=ffffff&y=100"
+      curl --unix-socket /var/run/pibox/framebuffer.sock "http://localhost/text?content=${i}+sec&size=36&color=CCCCCC&y=200"
+
+      sleep 1
+      ((i=i+1))
+    done
+  }
+
+  screen_timer &
+  SCREEN_TIMER_PID=$!
+
+  kubectl -n kube-system wait --for=condition=ready --timeout=120s pod -l app.kubernetes.io/name=traefik || kubernetes_failed_to_boot
+  kubectl -n kube-system wait --for=condition=ready --timeout=120s pod -l app=svclb-traefik || kubernetes_failed_to_boot
+  kubectl -n kube-system wait --for=condition=ready --timeout=120s pod -l k8s-app=kube-dns || kubernetes_failed_to_boot
+  kubectl -n kube-system wait --for=condition=ready --timeout=120s pod -l k8s-app=metrics-server || kubernetes_failed_to_boot
+
+  if [[ "$(kubectl -n kube-system get pods | sed 1d | egrep -v '(Completed|Running)')" != "" ]]; then
+    kubernetes_failed_to_boot
+  fi
+
+  kill $SCREEN_TIMER_PID
+  curl --unix-socket /var/run/pibox/framebuffer.sock "http://localhost/text?content=Installing+KubeSail+Agent&background=000000&size=46&color=ffffff"
   kubectl create -f https://api.kubesail.com/byoc
 fi
 
