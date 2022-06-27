6e47dae14fa729c2d57befd0ce0ba2e2c9161add
Seandon Mooy <seandon.mooy@gmail.com>
2022-06-27 16:49:20 -0700

    update framebuffer path and arch

--- update-framebuffer.sh
+++ update-framebuffer.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 if [ "$EUID" -ne 0 ]
   then echo "Please run as root"
   exit
@@ -6,12 +8,17 @@ fi
 echo "stopping pibox-framebuffer service"
 service pibox-framebuffer stop
 
-FB_VERSION=v9
+architecture="arm64"
+case $(uname -m) in
+  x86_64) architecture="amd64" ;;
+  arm)    dpkg --print-architecture | grep -q "arm64" && architecture="arm64" || architecture="arm" ;;
+esac
+FB_VERSION=v13
 FB_PATH=/opt/kubesail/pibox-framebuffer-$FB_VERSION
 rm -vf $FB_PATH
 echo "downloading pibox-framebuffer $FB_VERSION"
 if [[ ! -f $FB_PATH ]]; then
-    curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer
+    curl --connect-timeout 10 -sLo $FB_PATH https://github.com/kubesail/pibox-framebuffer/releases/download/$FB_VERSION/pibox-framebuffer-linux-${architecture}-$FB_VERSION
     chmod +x $FB_PATH
     rm /opt/kubesail/pibox-framebuffer
     ln -s $FB_PATH /opt/kubesail/pibox-framebuffer
