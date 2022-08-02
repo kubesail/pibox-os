4af089067fe5d2ad5fbb2821d936b5922efe29ae
Seandon Mooy <seandon.mooy@gmail.com>
2022-08-02 14:16:01 -0700

    Update kubesail-support.sh

--- kubesail-support.sh
+++ kubesail-support.sh
@@ -42,6 +42,9 @@ grep rancher /etc/fstab >> ${TMPFILE}
 echo -e "\n\nfindmnt -s ==============" >> ${TMPFILE}
 findmnt -s >> ${TMPFILE}
 
+echo -e "\n\nfindmnt /var/lib/rancher ==============" >> ${TMPFILE}
+findmnt /var/lib/rancher >> ${TMPFILE}
+
 echo -e "\n\nsystemctl status var-lib-rancher.mount ==============" >> ${TMPFILE}
 systemctl status var-lib-rancher.mount >> ${TMPFILE}
 
