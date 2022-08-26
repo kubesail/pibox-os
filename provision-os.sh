c15a833eabe67b788c97e835dac1956025f27ccf
Seandon Mooy <seandon.mooy@gmail.com>
2022-08-26 15:47:10 -0500

    Update provision-os.sh

--- provision-os.sh
+++ provision-os.sh
@@ -44,6 +44,7 @@ directory mask=0777
 public=no
 EOF
 
+mkdir -p /opt/kubesail
 curl -sLo /opt/kubesail/update-framebuffer.sh https://raw.githubusercontent.com/kubesail/pibox-os/main/update-framebuffer.sh
 chmod +x /opt/kubesail/update-framebuffer.sh
 /opt/kubesail/update-framebuffer.sh
