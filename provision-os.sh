d51a20f29c350c7efff4650833b04b1639c9f283
Dan Pastusek <dan@hexial.com>
2022-09-10 19:14:26 -0500

    escape shell

--- provision-os.sh
+++ provision-os.sh
@@ -155,16 +155,16 @@ cat <<EOF > /opt/kubesail/fix-unknown-pods.sh
 
 # Redirect standard out and standard error to a file.
 exec &> /var/log/fix-unknown-pods.log
-echo $(date +"%D %T")" Fixing any pods in Unknown state. Waiting 90s after boot..."
+echo \$(date +"%D %T")" Fixing any pods in Unknown state. Waiting 90s after boot..."
 
 (
     sleep 90
-    UNKNOWN_PODS=$(sudo kubectl get pods -A | grep Unknown)
-    if [ -z "${UNKNOWN_PODS}" ]; then
-      echo $(date +"%D %T")" No Unknown pods found."
+    UNKNOWN_PODS=\$(sudo kubectl get pods -A | grep Unknown)
+    if [ -z "\${UNKNOWN_PODS}" ]; then
+      echo \$(date +"%D %T")" No Unknown pods found."
     else  
-      echo $(date +"%D %T")" 90s elapsed, finding and fixing all Unknown pods."
-      for i in $(sudo k3s ctr c ls | awk '{print $1}'); do sudo k3s ctr c rm $i; done
+      echo \$(date +"%D %T")" 90s elapsed, finding and fixing all Unknown pods."
+      for i in \$(sudo k3s ctr c ls | awk '{print  \$1}'); do sudo k3s ctr c rm \$i; done
       sudo service k3s restart
       echo "All done - things should be back up and running in just a moment"
     fi
