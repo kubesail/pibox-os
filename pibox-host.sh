1a079fac33a725576b72cbfe4e0e88bbe50d42ec
Dan Pastusek <dan@hexial.com>
2024-03-06 09:35:55 -0800

    copy pi to root keys if requested

--- pibox-host.sh
+++ pibox-host.sh
@@ -2,9 +2,18 @@
 set -e
 set -x
 
-if [ "$EUID" -ne 0 ]
-  then echo "Please run as root"
+if [ "$EUID" -ne 0 ]; then
+  echo "Please run as root"
   exit
+fi
+
+# Ask user if they want to copy keys from pi to root
+# Default to yes
+echo "On initial setup of PiBox app, the 'pi' user will be deleted for security."
+read -p "Copy SSH keys from pi to root to retain remote access? (Y/n) " -n 1 -r
+echo
+if [[ $REPLY =~ ^[Yy]$ ]] || [[ $REPLY == "" ]]; then
+  cat /home/pi/.ssh/authorized_keys >>/root/.ssh/authorized_keys
 fi
 
 hostnamectl set-hostname pibox
@@ -13,7 +22,7 @@ sed -i 's/raspberrypi/pibox/' /etc/hosts
 # Unlimited bash history https://stackoverflow.com/a/19533853
 sed -i 's/HISTSIZE=1000//' /home/pi/.bashrc
 sed -i 's/HISTFILESIZE=2000//' /home/pi/.bashrc
-cat <<EOF >> /home/pi/.bashrc
+cat <<EOF >>/home/pi/.bashrc
 export HISTFILESIZE=
 export HISTSIZE=
 export HISTTIMEFORMAT="[%F %T] "
@@ -23,9 +32,8 @@ EOF
 
 cp /home/pi/.bashrc /root/.bashrc
 
-curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&\
-
-apt-get update -yqq
+curl -fsSL https://deb.nodesource.com/setup_20.x | bash - &&
+  apt-get update -yqq
 apt-get full-upgrade -yqq
 apt-get autoremove -yqq
 apt-get autoclean -yqq
@@ -73,13 +81,13 @@ sed -i "s/#SystemMaxFileSize.*/SystemMaxFileSize=10M/" /etc/systemd/journald.con
 systemctl restart systemd-journald.service
 
 # Add tmpfs at /tmp to reduce EMMC wear
-echo "tmpfs /var/tmp tmpfs defaults,noatime,nosuid,nodev,noexec,mode=0755,size=64M 0 0" >> /etc/fstab
+echo "tmpfs /var/tmp tmpfs defaults,noatime,nosuid,nodev,noexec,mode=0755,size=64M 0 0" >>/etc/fstab
 
 # Clone PiBox OS repo for building fan/display drivers
 rm -rf pibox-os
 git clone https://github.com/kubesail/pibox-os.git
 pushd pibox-os
-echo "PIBOX_RELEASE=$(git rev-parse --short HEAD)" > /etc/pibox-release
+echo "PIBOX_RELEASE=$(git rev-parse --short HEAD)" >/etc/pibox-release
 popd
 
 # Enable Fan Support
@@ -92,7 +100,7 @@ make && make install
 popd
 
 # Enable SPI for Display
-cat <<EOF >> /boot/firmware/config.txt
+cat <<EOF >>/boot/firmware/config.txt
 dtoverlay=spi0-1cs
 dtoverlay=dwc2,dr_mode=host
 hdmi_force_hotplug=1
@@ -103,20 +111,20 @@ EOF
 rm -rf pibox-os
 
 # Sysctl & limits
-echo "vm.swappiness=1" >> /etc/sysctl.conf
-echo "fs.file-max=1024000" >> /etc/sysctl.conf
+echo "vm.swappiness=1" >>/etc/sysctl.conf
+echo "fs.file-max=1024000" >>/etc/sysctl.conf
 sysctl -p
-echo "* soft nofile 8192" >> /etc/security/limits.conf
+echo "* soft nofile 8192" >>/etc/security/limits.conf
 
 # Swap
 swapoff -a
-systemctl mask  "dev-*.swap"
+systemctl mask "dev-*.swap"
 dphys-swapfile swapoff
 dphys-swapfile uninstall
 update-rc.d dphys-swapfile remove
 apt-get -yqq purge dphys-swapfile || true
 
-cat <<EOF > /usr/local/bin/rgb
+cat <<EOF >/usr/local/bin/rgb
 #!/bin/bash
 # RED
 echo "17" > /sys/class/gpio/export
@@ -143,13 +151,13 @@ done
 EOF
 chmod +x /usr/local/bin/rgb
 
-echo '' > /var/log/lastlog
-echo '' > /var/log/kern.log
-echo '' > /var/log/syslog
-echo '' > /var/log/user.log
-echo '' > /var/log/faillog
-echo '' > /var/log/messages
-echo '' > /var/log/auth.log
+echo '' >/var/log/lastlog
+echo '' >/var/log/kern.log
+echo '' >/var/log/syslog
+echo '' >/var/log/user.log
+echo '' >/var/log/faillog
+echo '' >/var/log/messages
+echo '' >/var/log/auth.log
 
 sed -i s/#PasswordAuthentication\ yes/PasswordAuthentication\ no/ /etc/ssh/sshd_config
 ssh-keygen -A
@@ -157,9 +165,8 @@ service ssh --full-restart
 
 # Place PiBox support public key in for local debugging at factory
 mkdir -p /root/.ssh
-echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCluY8xnwyrbfdxQebvawYI9qGvDqxsnKm8SYXKvJ8c0sO9dU3531dFHwkRLqdtiYQZl0xr+kuKSGGtFNJ1pUjJ+5t9tfwUZO3BL7DJkKYqUP4uuPg13Y1XFUsQw++IGW8pZNfcQjIDqYcaFd+Z0N7CCEVbHcBExaGYi/XaINl6EF+7aPSaymZrPXyzlVfFHbJlAN4+itWem4Ycm0oIu2Cw1YGXdap3RMrunjluYbHMWCJjpj1ipSpJsgyWq77+IX1Bom1pQypAZr1tu/lQyWFDtaJwcz3ZeSjqrTdFa5uxM4ppzVEgZEIQUKZmn/ETT9EWIsYugbhXKASdPdtx37ACpg0hkBZMBfffrOD9uhPjjhXhAzL3CCbGLqHdPj5SMtiBOJZ0+r8za0HK8NkTqpFNc9onKAtXXQr1Sajx4pd3tUPsyLDx4mROUxdOjRrO7xwmf4Ykxl7zy9a6W6NugJjupl4HF0tOm/P64gqSCjAZj0XpNDS+L8J2tVVVxqCpgYU=" >> /root/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCluY8xnwyrbfdxQebvawYI9qGvDqxsnKm8SYXKvJ8c0sO9dU3531dFHwkRLqdtiYQZl0xr+kuKSGGtFNJ1pUjJ+5t9tfwUZO3BL7DJkKYqUP4uuPg13Y1XFUsQw++IGW8pZNfcQjIDqYcaFd+Z0N7CCEVbHcBExaGYi/XaINl6EF+7aPSaymZrPXyzlVfFHbJlAN4+itWem4Ycm0oIu2Cw1YGXdap3RMrunjluYbHMWCJjpj1ipSpJsgyWq77+IX1Bom1pQypAZr1tu/lQyWFDtaJwcz3ZeSjqrTdFa5uxM4ppzVEgZEIQUKZmn/ETT9EWIsYugbhXKASdPdtx37ACpg0hkBZMBfffrOD9uhPjjhXhAzL3CCbGLqHdPj5SMtiBOJZ0+r8za0HK8NkTqpFNc9onKAtXXQr1Sajx4pd3tUPsyLDx4mROUxdOjRrO7xwmf4Ykxl7zy9a6W6NugJjupl4HF0tOm/P64gqSCjAZj0XpNDS+L8J2tVVVxqCpgYU=" >>/root/.ssh/authorized_keys
 
 # Clean bash history
 history -c && history -w
 exit
-
